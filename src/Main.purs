11cf61c2724677973657f1b7654143f6aa74c371
Yumenotimata <kouyayamaoka@gmail.com>
2025-01-31 13:28:56 +0900

    feat: intro free-monad

--- src/Main.purs
+++ src/Main.purs
@@ -4,12 +4,14 @@ import Control.Monad.Free
 import Data.Map
 import Prelude
 
-import Control.Monad.State (StateT, get, lift, modify_, runStateT)
-import Data.Array ((:))
+import Control.Monad.State (StateT, get, lift, modify_, put, runStateT)
+import Data.Array (last, tail, (:))
 import Data.Either (Either(..))
 import Data.Foldable (maximum)
+import Data.Generic.Rep (class Generic)
 import Data.Identity (Identity)
 import Data.Maybe (fromMaybe, maybe)
+import Data.Show.Generic (genericShow)
 import Data.String (joinWith)
 import Data.Tuple (Tuple, fst)
 import Data.Tuple.Nested ((/\))
@@ -18,69 +20,92 @@ import Effect.Console (log)
 
 main :: Effect Unit
 main = do
-  let e = runStateT encode { objs: [] }
-  log $ show e
-  -- let obj = mkDict [mkPair "Type" (Number 1.0), mkPair "Body" (mkArray [mkName "hello"])]
-  -- log $ show obj
+  let a = runStateT (runFreeM runPdf encode) { objs: [], prims: [], refs: [], lazy_ref: [], parent: 0 } 
+  log $ show a
+  log ""
 
-encode :: PdfM (Array Object)
-encode = do
-  pageTree <- mkObj $ Dictionary [mkPair "Kids" (Array []), mkPair "Count" (Number 1), mkPair "Type" (Name "Pages")]
-  pageTreeRef <- ref pageTree
-  documentCatalog <- mkObj $ Dictionary [mkPair "Pages" pageTreeRef, mkPair "Type" (Name "Catalog")]
-  pure $ []
+data PdfF a
+  = MkObj (Pdf Unit) (Object -> a)
+  | MkArray (Pdf Unit) a
+  | MkNull a
+  | MkRef Object a
 
-type PdfState = { objs :: Array Object }
-type PdfM a = StateT PdfState Identity a
+-- mkObj :: Pdf Unit -> Pdf Object
+-- mkObj = liftF $ MkObj 
 
-mkObj :: Primitive -> PdfM Ref
-mkObj prim = do
-  prev <- get
-  let latest = maybe 1 (\(Object obj) -> obj.number + 1) $ maximum prev.objs
-  let obj = Object { number: latest, gen: 0, inner: prim }
-  modify_ $ \s -> s { objs = s.objs <> [obj] }
-  pure latest
+type Pdf = Free PdfF
 
-data Object = Object { number :: Int, gen :: Int, inner :: Primitive }
-derive instance Eq Object
-derive instance Ord Object
+instance Functor PdfF where
+  map f (MkObj p k) = MkObj p (f <<< k)
+  map f (MkArray a x) = MkArray a (f x)
+  map f (MkNull a) = MkNull (f a)
+  map f (MkRef a x) = MkRef a (f x)
 
-instance Show Object where
-  show (Object obj) = "obj " <> show obj.number <> " " <> show obj.gen <> " " <> show obj.inner
+mkObj p = liftF $ MkObj p identity
 
-type Ref = Int
-ref :: Ref -> PdfM Primitive
-ref number = do
-  pure $ Ref number 0
+mkNull = liftF $ MkNull unit
+
+mkArray p = liftF $ MkArray p unit
+
+mkRef target = liftF $ MkRef target unit
 
 data Primitive
-  = Boolean Boolean
-  | Null
-  | Number Int
-  | Name String
-  | String String
+  = Null
+  | Ref Int
   | Array (Array Primitive)
-  | Dictionary (Array (Tuple String Primitive))
-  | Ref Int Int
-derive instance Eq Primitive
-derive instance Ord Primitive
--- mkArray = Array
-
--- mkName :: String -> Object
--- mkName = Name
-
-mkPair :: String -> Primitive -> Tuple String Primitive
-mkPair key value = key /\ value
-
--- mkDict :: Array (Tuple String Object) -> Object
--- mkDict = Dictionary
-
+derive instance Generic Primitive _
 instance Show Primitive where
-  show (Boolean bool) = show bool
-  show Null = "null"
-  show (Number number) = "number"
-  show (Name name) = "/" <> name
-  show (String string) = string
-  show (Array array) = "[" <> (joinWith " " $ map show array) <> "]"
-  show (Dictionary dict) = "<<\n" <> (joinWith "\n" $ map (\(key /\ value) -> "/" <> key <> " " <> show value) dict) <> "\n>>"
-  show (Ref number gen) = "ref " <> show number
+  show obj = genericShow obj
+
+type Object = { number :: Int, inner :: Array Primitive }
+
+type Reference = { src :: Int, dst :: Int }
+
+type PdfState = { objs :: Array Object, prims :: Array Primitive, refs :: Array Reference, lazy_ref :: Array Int, parent :: Int }
+
+runPdf :: forall a. PdfF (Pdf a) -> StateT PdfState Identity (Pdf a)
+runPdf f = go f
+  where
+    go (MkObj p contf) = do
+      -- store <- get
+      -- let prevNumber = maybe 0 (_.number) $ last store.objs
+      -- modify_ $ \s -> s { parent = prevNumber + 1 }
+      runFreeM runPdf p
+      -- put store
+      prev <- get
+      let prevNumber = maybe 0 (_.number) $ last prev.objs
+      let obj = { number: prevNumber + 1, inner: prev.prims }
+      let ref = prev.refs <> map (\dst -> { src: prevNumber + 1, dst }) prev.lazy_ref
+      modify_ $ \s -> s { objs = s.objs <> [obj], prims = [], refs = ref }
+      pure (contf obj)
+    go (MkArray p cont) = do
+      store <- get
+      put { objs: store.objs, prims: [], refs: store.refs, lazy_ref: store.lazy_ref, parent: store.parent }
+      runFreeM runPdf p
+      modify_ $ \s -> s { prims = store.prims <> [Array s.prims] }
+      pure cont
+    go (MkNull cont) = do
+      modify_ $ \prev -> 
+        prev { prims = prev.prims <> [Null] }
+      pure cont
+    go (MkRef target cont) = do
+      modify_ $ \prev -> 
+        prev { prims = prev.prims <> [Ref target.number] }
+      pure cont
+
+encode ∷ Pdf Unit
+encode = do
+  obj <- mkObj $ do
+    mkNull
+    mkNull
+    mkArray $ do
+      mkNull
+      mkArray $ do
+        mkNull
+
+  page <- mkObj $ do
+    mkNull
+    mkRef obj
+
+  pure unit
+  -- pure obj
